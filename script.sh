15a88c855af9052563bf7257e2d239461306d86c
Frank Archambault <farchamb1@gmail.com>
2019-12-02 14:53:19 -0500

    version beaucoup plus facile pour ip

--- script.sh
+++ script.sh
@@ -3,11 +3,6 @@
 #branche ip
 
 #Pour afficher l'addresse IP avant le login
-# sudo nano /etc/rc.local (fichier a aller changer)
-#avant exit 0:
 
-
-IP =$ (hostname -I) || true
-if [ "$IP" ]; then
-	printf "L'addresse IP est : %s\n" "$IP"
-fi
+touch \etc\issue #creation de issue (prelogin)
+echo "\4" \etc\issue #("\4" = adresse ip)

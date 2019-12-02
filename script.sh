e9fc79d0e9589ba5798c13d75d4743b47f7245b0
Frank Archambault <farchamb1@gmail.com>
2019-12-02 14:18:27 -0500

    premier essai de script pour IP

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 #branche ip
+
+#Pour afficher l'addresse IP avant le login
+# sudo nano /etc/rc.local (fichier a aller changer)
+#avant exit 0:
+
+
+IP =$ (hostname -I) || true
+if [ "$IP" ]; then
+	printf "L'addresse IP est : %s\n" "$IP"
+fi

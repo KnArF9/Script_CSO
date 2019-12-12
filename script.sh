31aa13dc37d5b1b9134638985a6fb4f745c994e8
Frank Archambault <farchamb1@gmail.com>
2019-12-12 13:23:09 -0500

    test

--- script.sh
+++ script.sh
@@ -23,4 +23,4 @@ echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
 echo 'AllowUsers Frank' >> /etc/ssh/sshd_config
 mkdir -p /.ssh
 touch /.ssh/authorized_keys
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwPoWFD2Fjm1dvTnNjGQUYPFt0uxhwYocLFw6kP8WQuQblS/p3ApophtUIzEBdxFGpvs5Y7SDvlib7/qaSNaMh8i60JOa86m+QkAP5vf26rPYVETNXtWoARt8scmGp/svoJpVWhQc2CFCI5/cNEJCZaMVvhSaFXum2P5EB/ulfEsR3Z9JXwerJ+cGkf+QEZMkIBW64EUl4HcYJ2qdqyoiu2/b6Ag2khVrn/k5Div2MQDy3OFfmK5c8bQPA1rHAk9gZ+szs3pD2pV4iy6LrL/6T1rB+XRItGabMGxNlGoo0b4BdVzDGEpJ/8mS4kBbt2ftes/K0mS08u+Kidvb5J+bf farch@XPS15-DE-FRANK' >> ~/.ssh/authorized_keys
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwPoWFD2Fjm1dvTnNjGQUYPFt0uxhwYocLFw6kP8WQuQblS/p3ApophtUIzEBdxFGpvs5Y7SDvlib7/qaSNaMh8i60JOa86m+QkAP5vf26rPYVETNXtWoARt8scmGp/svoJpVWhQc2CFCI5/cNEJCZaMVvhSaFXum2P5EB/ulfEsR3Z9JXwerJ+cGkf+QEZMkIBW64EUl4HcYJ2qdqyoiu2/b6Ag2khVrn/k5Div2MQDy3OFfmK5c8bQPA1rHAk9gZ+szs3pD2pV4iy6LrL/6T1rB+XRItGabMGxNlGoo0b4BdVzDGEpJ/8mS4kBbt2ftes/K0mS08u+Kidvb5J+bf farch@XPS15-DE-FRANK' >> /.ssh/authorized_keys
